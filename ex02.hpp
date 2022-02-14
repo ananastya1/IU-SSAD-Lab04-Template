2e8a30f46de81a142d059658fdcd8d7a63e999ae
ananastya1 <57675148+ananastya1@users.noreply.github.com>
2022-02-14 15:56:04 +0300

    Update ex02.hpp
    Name: Nastya Barabanova
    Email: a.barabanova@innopolis.university

--- ex02.hpp
+++ ex02.hpp
@@ -7,25 +7,30 @@
 std::ostringstream out2;
 using namespace std;
 
-/*Goal: practice multiple inheritance
-** Use Multiple inheritance Concept to calculate the Perimeter and Area of Rectangle.
-** Important: Perimeter and Area MUST be classes: have public methodsgetArea() and getPerimeter() .
-**--------------------------------------------------------
-** *****************Important*****************************
-** WHEN YOU'RE READY, GO to ex2(int, int), REMOVE THE TEMP return STATEMENT
-** & UNCOMMENT THE BLOCK OF STATEMENTS INSIDE THE FUNCTION.
-*/
+class Perimeter{
+    public double getPerimeter(double a, double b){
+        return 2 * (a + b);
+    }
+};
 
-// Put your code here
+
+class Area{
+    public double getArea(double a, double b){
+        return a * b;
+    }
+};
+
+
+class Rectangle : Area, Perimeter{
+
+};
 
 
 string ex2(int l, int w) {
-    // out2.str("");
-    // out2.clear();
-    // Rectangle rect;
-    // out2 << "Area: " << rect.getArea(l, w) << endl;
-    // out2 << "Perimeter: " << rect.getPerimeter(l, w) << endl;
-    // return out2.str();
-    return "WHEN YOU'RE READY REMOVE THIS LINE & UNCOMMENT THE BLOCK OF STATEMENTS ABOVE";
-
+     out2.str("");
+     out2.clear();
+     Rectangle rect;
+     out2 << "Area: " << rect.getArea(l, w) << endl;
+     out2 << "Perimeter: " << rect.getPerimeter(l, w) << endl;
+     return out2.str();
 }
