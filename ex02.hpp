d46b6a6e65611efb9808423a858726a91c362494
ananastya1 <57675148+ananastya1@users.noreply.github.com>
2022-02-14 15:45:49 +0300

    Update ex02.hpp

--- ex02.hpp
+++ ex02.hpp
@@ -19,13 +19,30 @@ using namespace std;
 // Put your code here
 
 
-string ex2(int l, int w) {
-    // out2.str("");
-    // out2.clear();
-    // Rectangle rect;
-    // out2 << "Area: " << rect.getArea(l, w) << endl;
-    // out2 << "Perimeter: " << rect.getPerimeter(l, w) << endl;
-    // return out2.str();
-    return "WHEN YOU'RE READY REMOVE THIS LINE & UNCOMMENT THE BLOCK OF STATEMENTS ABOVE";
+class Perimeter{
+    public double getPerimeter(double a, double b){
+        return 2 * (a + b);
+    }
+};
 
+
+class Area{
+    public double getArea(double a, double b){
+        return a * b;
+    }
+};
+
+
+class Rectangle : Area, Perimeter{
+
+};
+
+
+string ex2(int l, int w) {
+     out2.str("");
+     out2.clear();
+     Rectangle rect;
+     out2 << "Area: " << rect.getArea(l, w) << endl;
+     out2 << "Perimeter: " << rect.getPerimeter(l, w) << endl;
+     return out2.str();
 }
